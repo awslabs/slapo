f1253d393fda98ee8f7b3fc2d393e9d431999e4d
chhzh123 <hc676@cornell.edu>
2022-09-23 23:19:58 +0000

    [bert] Add training script

--- hf_bert.py
+++ hf_bert.py
@@ -1,10 +1,31 @@
+import time
 import transformers.utils.fx as fx
 from transformers import BertLMHeadModel, BertConfig
+import torch
 import ms
 
-bert = BertLMHeadModel(BertConfig(is_decoder=True))
+device = "cuda:0"
+
+bert = BertLMHeadModel(BertConfig(is_decoder=True)).to(device)
 bert.eval()
 gm = fx.symbolic_trace(bert)
 sch = ms.create_schedule(gm)
 print(sch.forward_ops)
-print(sch.modules)
+# print(sch.modules)
+print(bert.config.vocab_size)
+
+bs = 16
+seq_length = 512
+bert_input_dict = {
+    'input_ids': torch.zeros(bs, seq_length, dtype=torch.long, device=device).random_(bert.config.vocab_size),
+    'labels': torch.zeros(bs, seq_length, dtype=torch.long, device=device).random_(bert.config.vocab_size),
+    'attention_mask': torch.ones(bs, seq_length, device=device)}
+
+optimizer = torch.optim.SGD(bert.parameters(), lr=0.001)
+for i in range(5):
+    start_time = time.time()
+    output = bert(**bert_input_dict)
+    output.loss.backward()
+    optimizer.step()
+    elapsed_time = time.time() - start_time
+    print(f"Finish step {i}, time: {elapsed_time:.10f}s")

fe9d2004bc76fc7bdab3e70de335b10b56eaa642
Cody Yu <comaniac0422@gmail.com>
2023-01-12 01:56:59 +0000

    [License] Add license header

--- benchmark/tune_single_device.py
+++ benchmark/tune_single_device.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import os
 
 models = ["bert", "roberta", "gpt", "opt", "t5", "wideresnet"]

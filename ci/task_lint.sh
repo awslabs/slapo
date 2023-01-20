2e4ba9b385152a6b5be4b245fed0b2e8a4cef84f
Cody Yu <comaniac0422@gmail.com>
2023-01-20 01:56:26 +0000

    revert

--- ci/task_lint.sh
+++ ci/task_lint.sh
@@ -11,13 +11,13 @@ python3 scripts/lint/check_license_header.py HEAD~1
 python3 scripts/lint/check_license_header.py origin/main
 
 echo "Check Python formats using black..."
-#python3 -m pip install black==22.10.0
-#python3 -m pip install transformers==4.25.1 --no-deps
+python3 -m pip install black==22.10.0
+python3 -m pip install transformers==4.25.1 --no-deps
 bash ./scripts/lint/git-black.sh HEAD~1
 bash ./scripts/lint/git-black.sh origin/main
 
 echo "Running pylint on slapo"
-#python3 -m pip install pylint==2.14.0 astroid==2.11.6
+python3 -m pip install pylint==2.14.0 astroid==2.11.6
 python3 -m pylint slapo --rcfile=./scripts/lint/pylintrc
 
 echo "Running pylint on tests"

ae49b93599532a910ac756a5b743a460a3b264c8
chhzh123 <hc676@cornell.edu>
2023-01-24 07:32:39 +0000

    quick test

--- ci/task_unit_test.sh
+++ ci/task_unit_test.sh
@@ -8,14 +8,12 @@ set -o pipefail
 
 nvidia-smi -L
 
-echo "Running unit tests..."
-# -r: redirect the output of local rank 1 to None so that
-# only local rank 0's output is printed to the console.
-torchrun --nproc_per_node 2 -r 1:1 -m pytest tests
+# echo "Running unit tests..."
+# # -r: redirect the output of local rank 1 to None so that
+# # only local rank 0's output is printed to the console.
+# torchrun --nproc_per_node 2 -r 1:1 -m pytest tests
 
 echo "Downloading test data..."
-cd benchmark
-bash download_benchmark_dataset.sh
-cd ..
+bash benchmark/download_benchmark_dataset.sh
 echo "Running end-to-end tests..."
 python3 -m pytest -s tests/end2end.py
--- tests/end2end.py
+++ tests/end2end.py
@@ -29,12 +29,12 @@ def parse_log(impl, log_file):
 # fmt: off
 @pytest.mark.parametrize("model,impl,n_gpu,batch_size,seq_len,ckpt_ratio", [
     ("wideresnet-250M", "slapo-megatron", "1", "48", "512", "0.34"),
-    ("wideresnet-250M", "slapo-deepspeed", "4", "256", "512", "0.67"),
-    ("bert-large-uncased", "slapo-megatron", "2", "20", "512", "0"),
-    ("bert-large-uncased", "slapo-deepspeed", "2", "28", "512", "0"),
-    ("EleutherAI/gpt-neo-125M", "slapo-megatron", "2", "2", "512", "1.0"),
-    ("EleutherAI/gpt-neo-125M", "slapo-deepspeed", "4", "8", "512", "0.67"),
-    ("t5-base", "slapo-megatron", "4", "24", "1024", "0.67"),
+    # ("wideresnet-250M", "slapo-deepspeed", "4", "256", "512", "0.67"),
+    # ("bert-large-uncased", "slapo-megatron", "2", "20", "512", "0"),
+    # ("bert-large-uncased", "slapo-deepspeed", "2", "28", "512", "0"),
+    # ("EleutherAI/gpt-neo-125M", "slapo-megatron", "2", "2", "512", "1.0"),
+    # ("EleutherAI/gpt-neo-125M", "slapo-deepspeed", "4", "8", "512", "0.67"),
+    # ("t5-base", "slapo-megatron", "4", "24", "1024", "0.67"),
 ])
 # fmt: on
 def test_end2end(model, impl, n_gpu, batch_size, seq_len, ckpt_ratio):
@@ -43,7 +43,7 @@ def test_end2end(model, impl, n_gpu, batch_size, seq_len, ckpt_ratio):
         ckpt_ratio = "1.0"
     elif impl == "megatron":
         ckpt_ratio = "full"
-    cmd = f"cd benchmark && python3 bench_single_node.py {impl}"
+    cmd = f"python3 benchmark/bench_single_node.py {impl}"
     cmd += f" --model {model} --gpus {n_gpu} --seq-len {seq_len}"
     if "t5" in model:
         cmd += " --seq-len-dec 512"
@@ -54,7 +54,8 @@ def test_end2end(model, impl, n_gpu, batch_size, seq_len, ckpt_ratio):
     os.system(cmd)
     print("\n", flush=True)
     os.system("pwd")
-    error_code, samples_per_sec, _ = parse_log(impl, "benchmark/log.txt")
+    os.system("ls")
+    error_code, samples_per_sec, _ = parse_log(impl, "log.txt")
     print(f"\tThroughput: {samples_per_sec:.2f}")
     assert error_code == 0
 

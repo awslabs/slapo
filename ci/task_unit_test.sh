12410be530b14099d6149e4177308f2d91eff432
Cody Yu <comaniac0422@gmail.com>
2023-01-24 20:15:57 +0000

    test

--- ci/task_unit_test.sh
+++ ci/task_unit_test.sh
@@ -8,6 +8,10 @@ set -o pipefail
 
 nvidia-smi -L
 
+python3 -m pip install black==22.10.0
+python3 -m pip install transformers==4.25.1 --no-deps
+python3 -m pip install pylint==2.14.0 astroid==2.11.6 mock==4.0.3
+
 echo "Running unit tests..."
 # -r: redirect the output of local rank 1 to None so that
 # only local rank 0's output is printed to the console.

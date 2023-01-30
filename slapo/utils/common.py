dc6b5cf3411c62266b7bb4262035883eaab2994d
Cody Yu <comaniac0422@gmail.com>
2023-01-30 20:07:38 +0000

    support pipeline

--- slapo/utils/common.py
+++ slapo/utils/common.py
@@ -29,7 +29,7 @@ def get_hooks(mod):
     return hooks
 
 
-def transfer_hooks(old_mod, new_mod):
+def transfer_hooks(old_mod, new_mod, hook_types=None):
     """Transfer the hooks from old_mod to new_mod.
 
     Parameters
@@ -38,12 +38,18 @@ def transfer_hooks(old_mod, new_mod):
         The old module.
     new_mod : torch.nn.Module
         The new module.
+    hook_types : Optional[List[str]]
+        The types of hooks to transfer. If None, transfer all hooks.
     """
-    for hook in old_mod._forward_hooks.values():
-        new_mod.register_forward_hook(hook)
+    HOOK_TYPE_TO_ATTR = {
+        "fwd_pre": ("_forward_pre_hooks", "register_forward_pre_hook"),
+        "fwd_post": ("_forward_hooks", "register_forward_hook"),
+        "bwd_post": ("_backward_hooks", "register_backward_hook"),
+    }
+    if hook_types is None:
+        hook_types = ["fwd_pre", "fwd_post", "bwd_post"]
 
-    for hook in old_mod._forward_pre_hooks.values():
-        new_mod.register_forward_pre_hook(hook)
-
-    for hook in old_mod._backward_hooks.values():
-        new_mod.register_backward_hook(hook)
+    hook_attrs = [HOOK_TYPE_TO_ATTR[hook_type] for hook_type in hook_types]
+    for hook_attr, register_attr in hook_attrs:
+        for hook in getattr(old_mod, hook_attr).values():
+            getattr(new_mod, register_attr)(hook)

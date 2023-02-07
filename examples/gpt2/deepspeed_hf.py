b237aaba9e0876cdc29e4596a907efe8eebd8b48
Cody Yu <comaniac0422@gmail.com>
2023-02-07 00:55:42 +0000

    add gpt2

---
+++ examples/gpt2/deepspeed_hf.py
@@ -0,0 +1,328 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import random
+import numpy as np
+
+import deepspeed
+import torch
+import torch.distributed as dist
+from deepspeed.utils import RepeatingLoader
+from transformers import GPT2LMHeadModel, AutoConfig
+
+import slapo
+from slapo import set_random_seed
+from slapo.logger import get_logger
+from slapo.op import ParallelCrossEntropy
+from slapo.utils.report import report_memory
+
+from model import schedule_model
+from examples.utils import (
+    train_with_deepspeed_engine,
+    get_ds_config,
+    create_dist_group_for_pipeline,
+    generate_pipeline_cuts,
+)
+
+from examples.data_util import get_dataloader
+
+SINGLE_DEVICE_FOR_DEBUG = False
+
+logger = get_logger()
+
+
+def reconfig_model(args, model_config):
+    if args.hidden_size > 0:
+        model_config.hidden_size = args.hidden_size
+        model_config.num_hidden_layers = args.nlayers
+        model_config.num_attention_heads = args.num_attn_heads
+
+    if args.dropout > 0:
+        model_config.attn_pdrop = args.dropout
+        model_config.resid_pdrop = args.dropout
+        model_config.embd_pdrop = args.dropout
+
+    model_config.max_position_embeddings = args.seq_len
+
+    return model_config
+
+
+def train(args):
+    batch_size = args.batch_size
+    micro_batch_size = args.micro_batch_size
+
+    num_pp, num_mp = 1, 1
+    rank = args.local_rank
+    device = f"cuda:{rank}"
+    torch.cuda.set_device(rank)
+
+    # Configurations.
+    enable_pipeline = not SINGLE_DEVICE_FOR_DEBUG and not args.disable_pipeline
+    if args.disable_schedule and args.checkpoint not in [0.0, 1.0]:
+        raise ValueError("checkpoint must be 0.0 or 1.0 with disable_schedule")
+    use_default_ckpt = args.checkpoint == 1.0 and args.disable_schedule
+
+    topology, group = None, None
+    if not SINGLE_DEVICE_FOR_DEBUG:
+        deepspeed.init_distributed(dist_backend="nccl")
+        logger.info("Use deepspeed to initialize", ranks=0)
+        if enable_pipeline:
+            # num_pp, num_mp = 4, 2 # For single node testing.
+            num_pp = args.pmp
+            num_mp = args.tmp
+        else:
+            logger.info("Pipeline disabled", ranks=0)
+            num_pp = 1
+            num_mp = args.tmp
+
+        topology, group = create_dist_group_for_pipeline(num_pp, num_mp)
+
+        # FIXME: Pytorch _coalescing_manager requires all the ranks to join
+        # if that is the first collective call in the given group.
+        # We use the following broadcast as the first call for workaround,
+        # and it will be removed once we implement the features to synchonrize
+        # the model parameters during initialization.
+        x = torch.tensor(0, device=torch.cuda.current_device())
+        dist.broadcast(x, src=0)
+
+    logger.info(f"TMP {num_mp}, PMP {num_pp}", ranks=[0])
+    # https://huggingface.co/EleutherAI/gpt-neo-2.7B/blob/main/config.json
+    config = AutoConfig.from_pretrained(args.model_name)
+    # FIXME: This model has vocab size 50257 that cannot be sharded by 2,
+    # so we pad it to 50258 in this example. In practice, the tokenizer
+    # should be used to pad the vocab size to a multiple of 2.
+    config.vocab_size = (config.vocab_size // 8 + 1) * 8
+    config.use_cache = False
+    config.gradient_checkpointing = use_default_ckpt
+    config = reconfig_model(args, config)
+    logger.info(config, ranks=[0])
+
+    report_memory(msg="Before creating model")
+    with slapo.init_empty_weights(enable=enable_pipeline):
+        model = GPT2LMHeadModel(config)
+    report_memory(msg="After creating model")
+
+    # Evenly partition layers for pipelining.
+    if enable_pipeline:
+        pipeline_cuts = generate_pipeline_cuts(config.num_hidden_layers, num_pp)
+    elif SINGLE_DEVICE_FOR_DEBUG:
+        pipeline_cuts = generate_pipeline_cuts(config.num_hidden_layers, 4)
+    else:
+        pipeline_cuts = []
+    logger.info(f"Pipeline cuts: {pipeline_cuts}", ranks=0)
+
+    if args.disable_schedule:
+        assert not enable_pipeline
+        sch = slapo.create_schedule(model, group=group)
+    else:
+        sch = schedule_model(
+            model,
+            config,
+            prefix="transformer",
+            ckpt_ratio=args.checkpoint,
+            bcast_input=True,
+            fp16=args.fp16,
+            bf16=args.bf16,
+            group=group,
+            pipeline_cuts=pipeline_cuts,
+            delay_init=enable_pipeline,
+            sequence_parallel=args.sequence_parallel,
+        )
+    tp_rank = sch.rank
+
+    loss_fct = ParallelCrossEntropy(group=group)
+
+    def loss_fn(outputs, labels):
+        prediction_scores = outputs.to(torch.float32)
+        shifted_prediction_scores = prediction_scores[..., :-1, :].contiguous()
+        labels = labels[..., 1:].contiguous()
+        lm_loss = loss_fct(shifted_prediction_scores, labels)
+        lm_loss = lm_loss.contiguous().mean()
+        return lm_loss
+
+    if enable_pipeline:
+        # FIXME: is mbs=1 correct?
+        batch_size = 16 if batch_size is None else batch_size
+        micro_batch_size = 4 if micro_batch_size is None else micro_batch_size
+        zero_opt_stage = 0
+        ds_config_dict = get_ds_config(
+            batch_size,
+            micro_batch_size,
+            args.fp16,
+            zero_opt_stage,
+            "Pipeline",
+            args.bf16,
+        )
+
+        model, _ = slapo.build(
+            sch,
+            topology=topology,
+            target="deepspeed",
+            config=ds_config_dict,
+            loss_fn=loss_fn,
+            init_weights=model._init_weights,
+        )
+    else:
+        if batch_size is not None and micro_batch_size is None:
+            micro_batch_size = batch_size // args.world_size
+        if batch_size is None and micro_batch_size is not None:
+            batch_size = micro_batch_size * args.world_size
+
+        # if the TP == 1 use zero 3, otherwise use stage-1 optimizer
+        zero_opt_stage = 3 if args.tmp == 1 else 1
+        logger.info(f"BS={batch_size}, MBS={micro_batch_size}", ranks=0)
+        ds_config_dict = get_ds_config(
+            batch_size,
+            micro_batch_size,
+            args.fp16,
+            zero_opt_stage,
+            f"ZeRO-{zero_opt_stage}",
+            args.bf16,
+        )
+        model, _ = slapo.build(
+            sch,
+            topology=topology,
+            target="deepspeed",
+            config=ds_config_dict,
+            init_weights=model._init_weights,
+        )
+        model = model.to(device)
+    report_memory(msg="After building model")
+
+    if args.disable_pipeline or args.sequence_parallel:
+        set_random_seed(2013, model.mpu.get_data_parallel_rank(), None, tp_rank)
+    else:
+        set_random_seed(
+            2013,
+            model.mpu.get_data_parallel_rank(),
+            model.mpu.get_pipe_parallel_rank(),
+            tp_rank,
+        )
+
+    # for now always use seq_length 1024
+    # TODO: make the dataloader generic to different sequence length
+    train_loader, _ = get_dataloader(
+        args.model_name,
+        micro_batch_size,
+        enable_pipeline,
+        mpu=model.mpu,
+        max_seq_length=args.seq_len,
+    )
+
+    loader = RepeatingLoader(train_loader)
+
+    num_iters = args.iter_nums
+    if enable_pipeline:
+        data_iter = iter(loader)
+        for _ in range(num_iters):
+            model.train_batch(data_iter=data_iter)
+    else:
+        # use the Parallel Loss
+        loss_fn = None if args.tmp == 1 else loss_fn
+        train_with_deepspeed_engine(
+            model,
+            loader,
+            steps=num_iters,
+            loss_fn=loss_fn,
+        )
+
+
+if __name__ == "__main__":
+    n_gpus = torch.cuda.device_count()
+    parser = argparse.ArgumentParser()
+    # This is passed in via cmd
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument("--world_size", type=int, default=n_gpus)
+    parser.add_argument("--iter_nums", type=int, default=40)
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="gpt2-xl", # 1.5B
+        help="Model name",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        type=float,
+        default=0.0,
+        help="Activation checkpointing ratio. 1.0 means all",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=None,
+        help="Total batch size",
+    )
+    parser.add_argument(
+        "--micro_batch_size",
+        type=int,
+        default=None,
+        help="Micro batch size per GPU",
+    )
+    parser.add_argument(
+        "--seq_len",
+        type=int,
+        default=1024,
+        help="Sequence length",
+    )
+    parser.add_argument(
+        "--disable_pipeline",
+        action="store_true",
+        help="Disable pipeline and only use ZeRO-3",
+    )
+    parser.add_argument(
+        "--disable_schedule",
+        action="store_true",
+        help="Disable Slapo schedule (only applicable with --disable-pipeline)",
+    )
+    parser.add_argument(
+        "--hidden-size",
+        type=int,
+        default=-1,
+        help="Config hidden size of the model, if it is negative value,"
+        " it uses default value associated with the model name",
+    )
+    parser.add_argument(
+        "--nlayers", type=int, default=-1, help="Number of transformer layers"
+    )
+    parser.add_argument(
+        "--num-attn-heads", type=int, default=-1, help="Number of attention heads"
+    )
+    parser.add_argument(
+        "--dropout", type=float, default=0.0, help="Dropout probability"
+    )
+    parser.add_argument(
+        "--pmp", type=int, default=2, help="Pipeline model parallel size"
+    )
+    parser.add_argument("--tmp", type=int, default=8, help="Tensor parallel size")
+    parser.add_argument(
+        "--sequence_parallel",
+        action="store_true",
+        help="Sequence parallelism is enabled",
+    )
+    parser.add_argument(
+        "--fp16",
+        action="store_true",
+        help="fp16 is enabled. fp16 is enabled by default",
+    )
+    parser.add_argument(
+        "--bf16",
+        action="store_true",
+        help="bf16 is enabled",
+    )
+    args = parser.parse_args()
+
+    if args.fp16 and args.bf16:
+        raise ValueError(
+            f"fp16={args.fp16} and bf16={args.bf16} cannot be enabled at the same time"
+        )
+    elif not args.fp16 and not args.bf16:
+        args.fp16 = True
+        logger.info("fp16 is enabled by default", ranks=0)
+
+    if args.hidden_size > 0:
+        assert args.nlayers > 0, "must have nlayers > 0"
+        assert args.num_attn_heads > 0, "must have num_attn_heads > 0"
+
+    # The main entry point is called directly without using subprocess
+    train(args)
---
+++ examples/gpt2/schedule.py
@@ -0,0 +1,437 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+import inspect
+
+import torch.nn as nn
+from torch.distributed import distributed_c10d as dist
+
+import slapo
+from slapo import init_empty_weights
+from slapo.op import FlashSelfAttention, FlashAttentionOp, FusedMLP
+from slapo import init_empty_weights, get_cuda_rng_tracker
+
+
+def replace_attention(
+    sch,
+    config,
+    attn_path="h.N.attn",
+    delay_init=True,
+    disable_flash_attn=False,
+):
+    """Replace the attention module with flash attention.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the entire model.
+    config : GPT2Config
+        The model configuration.
+    attn_path : str
+        The path to the attention module.
+    delay_init : bool
+        Whether to delay the initialization of the new module.
+    disable_flash_attn : bool
+        Whether to disable the flash attention.
+    """
+    attn_op_name = "native_xformers" if disable_flash_attn else "triton"
+    init_config = dict(
+        hidden_size=config.hidden_size,
+        num_attention_heads=config.num_attention_heads,
+        is_decoder=True,
+        attn_pdrop=config.attn_pdrop,
+        resid_pdrop=config.resid_pdrop,
+        attn_op_name=attn_op_name,
+        fused_qkv=True,
+        world_size=sch.world_size,
+    )
+
+    class SelfAttention(nn.Module):
+        """A wrapper to align the original GPTNeoAttention forward signature."""
+
+        def __init__(self, **kwargs):
+            super().__init__()
+            try:
+                self.module = FlashSelfAttention(**kwargs)
+            except Exception as err:
+                if kwargs["attn_op_name"] == "native_xformers":
+                    raise RuntimeError(
+                        f"Failed to create native attention: {err}"
+                    ) from None
+
+                # Failed to use the triton kernel. This may due to unsupported
+                # GPU (< sm_75) or flash-attention is not installed. Fallback
+                # to xFormers' cutlass.
+                kwargs["attn_op_name"] = "cutlass"
+                self.module = FlashSelfAttention(**kwargs)
+
+        def forward(
+            self,
+            hidden_states,
+            layer_past=None,
+            attention_mask=None,
+            head_mask=None,
+            encoder_hidden_states=None,
+            encoder_attention_mask=None,
+            use_cache=False,
+            output_attentions=False,
+        ):
+            outputs = self.module(
+                hidden_states,
+                layer_past,
+                attention_mask,
+                head_mask,
+                encoder_hidden_states,
+                encoder_attention_mask,
+                use_cache,
+                output_attentions,
+            )
+            # FIXME: The original output is (hidden_states, None) where the None
+            # is present_key_value and only used by in inference.
+            return outputs[:1]
+
+    cnt = 0
+    for idx in range(config.num_hidden_layers):
+        sub_sch = sch[attn_path.replace("N", str(idx))]
+        with init_empty_weights(enable=delay_init):
+            new_mod = SelfAttention(**init_config)
+        sub_sch.replace(new_mod)
+        cnt += 1
+
+    return cnt
+
+
+def replace_mlp(
+    sch,
+    config,
+    path="h.N.mlp",
+    delay_init=True,
+):
+    """Replace the MLP module with a fused MLP module.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the entire model.
+    config : GPT2Config
+        The model configuration.
+    path : str
+        The path to the MLP module.
+    delay_init : bool
+        Whether to delay the initialization of the new module.
+    """
+    for idx in range(config.num_hidden_layers):
+        prefix = path.replace("N", str(idx))
+        sub_sch = sch[prefix]
+        hidden_size, inter_size = sub_sch.mod.c_fc.weight.shape
+        with init_empty_weights(enable=delay_init):
+            new_mod = FusedMLP(
+                hidden_size,
+                inter_size,
+                config.activation_function,
+                config.resid_pdrop,
+            )
+        sub_sch.replace(new_mod)
+
+
+def gen_embedding_hooks(sch, vocab_size):
+    """Generate hooks for input embedding layer to deal with word embedding sharding.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the entire model.
+    vocab_size : int
+        The total vocabulary size.
+
+    Returns
+    -------
+    tuple(callable, callable)
+        The forward pre-hook and post-hook for the embedding layer.
+    """
+    vocab_start_index = sch.rank * vocab_size // sch.world_size
+    vocab_end_index = (sch.rank + 1) * vocab_size // sch.world_size
+
+    def fwd_pre_hook(_module, _input):
+        # Mask the input
+        input_mask = (_input[0] < vocab_start_index) | (_input[0] >= vocab_end_index)
+        masked_input = _input[0].clone() - vocab_start_index
+        masked_input[input_mask] = 0
+        return masked_input
+
+    def fwd_post_hook(_module, _input, output):
+        # Mask the output embedding
+        input_mask = (_input[0] < vocab_start_index) | (_input[0] >= vocab_end_index)
+        output[input_mask, :] = 0.0
+        # Reduce across all the model parallel GPUs
+        output = slapo.sharding.reduce_forward_output(output, sch.group)
+        return output
+
+    return fwd_pre_hook, fwd_post_hook
+
+
+def fix_attention_mask_shape(sch):
+    """A utility function to fix the attention mask shape.
+    The input attention mask shape is (B, 1, 1, S) where S is the sequence length.
+    However, xFormers kernels expect (B, H, S, S) where H is the number of heads.
+    Since we shard H to be H // world_size, we need to fix this expand op.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the entire model.
+    """
+    ops = sch.find_node(
+        lambda node: node.op == "call_method" and node.target == "expand"
+    )
+
+    def new_expand(tensor, *args):
+        # (B, 1, 1, S) -> (B, H, S, S)
+        assert len(args) == 4
+        out = tensor.expand(args[0], args[1] // sch.world_size, *args[2:])
+        return out.contiguous()
+
+    for op in ops:
+        sch.replace(new_expand, op[1])
+
+
+class AttentionOpWithRNG(FlashAttentionOp):
+    def forward(self, query_layer, key_layer, value_layer, attention_mask, p):
+        with get_cuda_rng_tracker().fork():
+            return super().forward(
+                query_layer, key_layer, value_layer, attention_mask, p
+            )
+
+
+def shard(
+    sch,
+    head_sch,
+    config,
+    shard_target,
+    sequence_parallel=False,
+):
+    """Shard the model for tensor parallelism. This function assumes
+    the attention layers are already replaced with the Slapo ops.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the transformer model.
+    head_sch : slapo.Schedule
+        The schedule of the lm_head.
+    config: GPT2Config
+        The configuration of the model.
+    shard_target : list[str]
+        The sharding target. This function shards the corresponding layers
+        specified in this target. It could include "embed", "attention", "mlp".
+    sequence_parallel : bool
+        Whether to use sequence parallelism. Default False.
+    """
+
+    if sch.world_size == 1:
+        return
+
+    # Shard input embedding.
+    if "embed" in shard_target:
+        word_embed_name, pos_embed_name, final_ln_name = "wte", "wpe", "ln_f"
+
+        sch[word_embed_name].shard("weight", axis=0)
+        fwd_pre_hook, fwd_post_hook = gen_embedding_hooks(sch, config.vocab_size)
+        sch[word_embed_name].sync(mode="fwd_pre", sync_op_or_fn=fwd_pre_hook)
+        sch[word_embed_name].sync(mode="fwd_post", sync_op_or_fn=fwd_post_hook)
+
+        if sequence_parallel:
+            sch[word_embed_name].sync(mode="fwd_post", sync_op_or_fn="scatter", axis=1)
+            sch[pos_embed_name].sync(mode="fwd_post", sync_op_or_fn="scatter", axis=1)
+            sch[final_ln_name].sync(
+                mode="fwd_post",
+                sync_op_or_fn="all_gather",
+                axis=1,
+                tensor_parallel_output_grad=False,
+            )
+
+        # Shard output embedding.
+        if head_sch is not None:
+            head_sch.shard("weight", axis=0)
+            head_sch.sync(mode="bwd_post", sync_op_or_fn="all_reduce")
+
+    # Shard attention.
+    if "attention" in shard_target:
+        path = "h.N.attn"
+
+        for idx in range(config.num_hidden_layers):
+            sub_sch = sch[path.replace("N", str(idx))]
+            sub_sch["module.qkv"].shard("weight", axis=0)
+            sub_sch["module.qkv"].shard("bias", axis=0)
+            sub_sch["module.out_proj"].shard("weight", axis=1)
+
+            # Fix attention mask shape to consider the sharded size. This requires
+            # the attention module to be in static graph, and thus we need to trace it.
+            sub_sch.trace(
+                tracer="pytorch",
+                leaf_modules=["FlashAttentionOp"],
+                concrete_args={
+                    "layer_past": None,
+                    "head_mask": None,
+                    "encoder_hidden_states": None,
+                    "encoder_attention_mask": None,
+                    "use_cache": False,
+                    "output_attentions": False,
+                },
+            )
+            fix_attention_mask_shape(sub_sch["module"])
+
+            if sequence_parallel:
+                sub_sch["module.qkv"].sync(
+                    mode="fwd_pre", sync_op_or_fn="all_gather", axis=1
+                )
+
+                sub_sch["module.out_proj"].sync(
+                    mode="fwd_post", sync_op_or_fn="reduce_scatter", axis=1
+                )
+            else:
+                # Shard qkv and output projection.
+                sub_sch["module.qkv"].sync(mode="bwd_post", sync_op_or_fn="all_reduce")
+                sub_sch["module.out_proj"].sync(
+                    mode="fwd_post", sync_op_or_fn="all_reduce"
+                )
+
+                # In this case, the attention dropout in between has to
+                # use different random seeds.
+                new_op = AttentionOpWithRNG(
+                    sub_sch["module"]["attn_op"].mod.attn_op_name,
+                    sub_sch["module"]["attn_op"].mod.apply_causal_mask,
+                    sub_sch["module"]["attn_op"].mod.scale,
+                )
+                sub_sch["module"]["attn_op"].replace(new_op)
+
+    # Shard MLP.
+    if "mlp" in shard_target:
+        path = "h.N.mlp"
+
+        for idx in range(config.num_hidden_layers):
+            prefix = path.replace("N", str(idx))
+            sub_sch = sch[prefix]
+
+            # The name of first linear, the linear that bias belongs to,
+            # and last linear layers.
+            # fc_names = ["c_fc", "c_fc", "c_proj"]
+            # FIXME: If MLP is not replaced, the weights in GPT2MLP are transposed
+            # and we don't handle that right now.
+            assert "act" in sub_sch
+            # When the MLP is replaced, the bias of the first linear
+            # is splitted to the new module of fused activation layer.
+            fc_names = ["fc_in", "act", "fc_out"]
+
+            sub_sch[fc_names[0]].shard("weight", axis=0)
+            sub_sch[fc_names[1]].shard("bias", axis=0)
+            sub_sch[fc_names[2]].shard("weight", axis=1)
+
+            if sequence_parallel:
+                sub_sch[fc_names[0]].sync(
+                    mode="fwd_pre", sync_op_or_fn="all_gather", axis=1
+                )
+                sub_sch[fc_names[2]].sync(
+                    mode="fwd_post", sync_op_or_fn="reduce_scatter", axis=1
+                )
+            else:
+                sub_sch[fc_names[0]].sync(mode="bwd_post", sync_op_or_fn="all_reduce")
+                sub_sch[fc_names[2]].sync(mode="fwd_post", sync_op_or_fn="all_reduce")
+
+
+def checkpoint(sch, config, path="h.N", ckpt_ratio=1.0):
+    """Add activation checkpointing to the model. The ckpt_ratio specifies
+    the ratio of the attention layers to be checkpointed. For example, if
+    ckpt_ratio is 0.5, then half of the attention layers will be checkpointed.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the model.
+    config : GPT2Config
+        The configuration of the model.
+    path : str
+        The path to the attention layer. Default: "h.N.attn".
+    ckpt_ratio : float
+        The ratio of the attention layers to be checkpointed. Default: 1.0.
+    """
+    if ckpt_ratio == 0.0:
+        return
+
+    def order_args_fn(*args, **kwargs):
+        assert len(args) == 1
+        attention_mask = kwargs.get("attention_mask", None)
+        head_mask = kwargs.get("head_mask", None)
+        encoder_hidden_states = kwargs.get("encoder_hidden_states", None)
+        encoder_attention_mask = kwargs.get("encoder_attention_mask", None)
+        output_attentions = kwargs.get("output_attentions", False)
+        # Forwards: (
+        #   hidden_states,
+        #   layer_past,
+        #   attention_mask,
+        #   head_mask,
+        #   encoder_hidden_states,
+        #   encoder_attention_mask,
+        #   use_cache,
+        #   output_attentions
+        # )
+        return (
+            args[0],
+            None,
+            attention_mask,
+            head_mask,
+            encoder_hidden_states,
+            encoder_attention_mask,
+            False,
+            output_attentions,
+        )
+
+    n_ckpt = int(config.num_hidden_layers * ckpt_ratio)
+    for idx in range(n_ckpt):
+        sch[path.replace("N", str(idx))].checkpoint(order_args_fn=order_args_fn)
+    return n_ckpt
+
+
+def broadcast_input(sch):
+    """Add a hook in the beinning of the model to broadcast the input.
+    This is used when tensor parallelism is used and the runtime does not
+    broadcast the input automatically.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule of the model.
+    """
+    group_src_rank = dist.get_global_rank(sch.group, 0)
+
+    def _broadcast_input(module, inputs):
+        for inp in inputs:
+            if inp is not None:
+                dist.broadcast(inp, src=group_src_rank, group=sch.group)
+        return inputs
+
+    sch.sync(mode="fwd_pre", sync_op_or_fn=_broadcast_input)
+
+
+def pipeline(sch, prefix, pipeline_cuts):
+    """Trace the top module of the model and cut the pipeline stages.
+
+    Parameters
+    ----------
+    sch : slapo.Schedule
+        The schedule to be cut.
+    prefix : str
+        The prefix of the top module.
+    pipeline_cuts : list[str]
+        The list of attention layer index indicating cut points.
+    """
+    input_names = ["input_ids", "attention_mask", "position_ids"]
+    sig = inspect.signature(sch.mod.forward)
+    concrete_args = {
+        p.name: p.default for p in sig.parameters.values() if p.name not in input_names
+    }
+    sch.trace_for_pipeline(
+        f"{prefix}", tracer="huggingface", concrete_args=concrete_args
+    )
+    _prefix = f"{prefix}." if prefix else ""
+    for cut in pipeline_cuts:
+        sch[f"{_prefix}h.{cut}"].cut_pipeline_stage()

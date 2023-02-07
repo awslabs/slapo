8a2fc9149721dfd8a6394c34cdce66eb03f646fa
Cody Yu <comaniac0422@gmail.com>
2023-02-07 02:30:54 +0000

    fix order

--- ci/task_unit_test.sh
+++ ci/task_unit_test.sh
@@ -8,6 +8,17 @@ set -o pipefail
 
 nvidia-smi -L
 
+# Remove this path when xFormers fixes this issue.
+echo "Applying xFormers path..."
+XFORMER_PATH=`python3 -c "import xformers, pathlib; print(pathlib.Path(xformers.__path__[0]).parent)"`
+cp scripts/xformers_patch $XFORMER_PATH
+pushd $XFORMER_PATH
+git config --global --add safe.directory $XFORMER_PATH
+git reset --hard
+git apply xformers_patch
+git --no-pager diff
+popd
+
 echo "Running unit tests..."
 # torchrun:
 #   -r: redirect the output of local rank 1 to None so that
@@ -19,17 +30,6 @@ torchrun --nproc_per_node 2 -r 1:1 -m pytest -rxXs -p "no:randomly" tests
 
 echo "Downloading test data..."
 bash benchmark/download_benchmark_dataset.sh
-
-# Remove this path when xFormers fixes this issue.
-echo "Applying xFormers path..."
-XFORMER_PATH=`python3 -c "import xformers, pathlib; print(pathlib.Path(xformers.__path__[0]).parent)"`
-cp scripts/xformers_patch $XFORMER_PATH
-pushd $XFORMER_PATH
-git config --global --add safe.directory $XFORMER_PATH
-git reset --hard
-git apply xformers_patch
-git --no-pager diff
-popd
 
 echo "Running end-to-end tests..."
 python3 -m pytest -s -p "no:randomly" tests/end2end.py

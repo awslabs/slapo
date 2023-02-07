63fb4f84bffd6a0fd5deafacb98c44b54dc57777
chhzh123 <hc676@cornell.edu>
2023-02-07 03:26:00 +0000

    Add flag to albert

--- examples/albert/model.py
+++ examples/albert/model.py
@@ -28,6 +28,7 @@ def schedule_model(
     group=None,
     bcast_input=False,
     pipeline_cuts=None,
+    separate_op_fusion=False,
     delay_init=True,
 ):
     logger.info("Scheduling Albert", ranks=0)
@@ -50,7 +51,10 @@ def schedule_model(
     )
     logger.info(f"Replace {cnt} attention patterns", ranks=0)
 
-    fuse_bias_gelu(sch[prefix], config)
+    # Operator fusion
+    if separate_op_fusion:
+        fuse_bias_gelu(sch[prefix], config)
+        logger.info(f"Fused Bias+GeLU", ranks=0)
 
     # Shard other parameters if MP group > 1.
     if sch.world_size > 1:

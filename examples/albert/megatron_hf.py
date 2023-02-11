0c3351b801f7c4b8dca41a7752854561fca3ce96
chhzh123 <hc676@cornell.edu>
2023-02-11 02:58:07 +0000

    Move albert

--- examples/albert/megatron_hf.py
+++ examples/albert/megatron_hf.py
@@ -46,7 +46,7 @@ def get_model(
     if "slapo" in impl:
         import slapo
         from slapo.utils.report import report_memory
-        from model import schedule_model
+        from slapo.model_schedule.albert import schedule_model
 
         report_memory()
         with slapo.init_empty_weights(enable=delay_init):

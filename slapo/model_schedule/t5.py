56f7e1615c9dc44cccc420f16b5189f05806f123
chhzh123 <hc676@cornell.edu>
2023-02-12 00:15:16 +0000

    Fix pylint

--- slapo/model_schedule/t5.py
+++ slapo/model_schedule/t5.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """HuggingFace T5 with model schedule."""
-# pylint: disable=logging-fstring-interpolation
+# pylint: disable=logging-fstring-interpolation, import-error
 
 import inspect
 

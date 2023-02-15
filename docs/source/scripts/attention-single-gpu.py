e67a06150b0ca9369e8ffef562ec45e1ab63907c
chhzh123 <hc676@cornell.edu>
2023-02-15 19:23:59 +0000

    [docs] Add flashattn

--- docs/source/scripts/attention-single-gpu.py
+++ docs/source/scripts/attention-single-gpu.py
@@ -1,13 +1,13 @@
 """
 .. currentmodule:: slapo
 
-Optimize Attention Module on A Single GPU
-=========================================
+Optimize Attention Module on A Single Device
+============================================
 
 This guide uses the `Attention <https://arxiv.org/abs/1706.03762>`_ module,
 the core and most time-consuming module in Transformer-based models, as an
 example to show how we can leverage Slapo to optimize its performance on
-a single GPU.
+a single device.
 """
 
 # %%
@@ -20,17 +20,19 @@ import torch.nn.functional as F
 import slapo
 
 # %%
-# The Attention module is consisted of SelfAttention and Projection modules, where
-# SelfAttention takes in the hidden states and pass it through three diffferent
+# The Attention module consists of SelfAttention and Projection modules, where
+# SelfAttention takes in the hidden states and passes it through three different
 # linear layers to generate the query, key and value tensors. Then, those tensors
 # will be performed the following scaled dot-product attention:
-# 
-# $$ \mathrm{CoreAttention}(Q, K, V) = \mathrm{softmax}\left(\frac{QK^T}{\sqrt(d_k)}\right) * V $$
-# 
-# where $d_k$ is the hidden dimension. Finally, the output of the attention module
-# will be passed through a linear projection layer, added with the residual
+#
+# .. math::
+#    \\mathrm{CoreAttention}(Q, K, V) = \\mathrm{softmax}\\left(\\frac{QK^{\\mathrm{T}}}{\\sqrt(d_k)}\\right) * V
+#
+# where :math:`d_k` is the hidden dimension. Finally, the output of the attention
+# module will be passed through a linear projection layer, added with the residual
 # connection, and conducted a layer norm to generate the final output.
 # The following code shows the implementation of the Attention module.
+
 
 def scaled_dot_product(q, k, v):
     # (bs, head, seq, hs // head)
@@ -45,13 +47,12 @@ def scaled_dot_product(q, k, v):
 
 
 class SelfAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size, n_heads):
         super().__init__()
-        self.q_proj = nn.Linear(1024, 1024)
-        self.k_proj = nn.Linear(1024, 1024)
-        self.v_proj = nn.Linear(1024, 1024)
-        self.dropout = nn.Dropout(0.1)
-        self.n_heads = 16
+        self.q_proj = nn.Linear(hidden_size, hidden_size)
+        self.k_proj = nn.Linear(hidden_size, hidden_size)
+        self.v_proj = nn.Linear(hidden_size, hidden_size)
+        self.n_heads = n_heads
 
     def permute_for_scores(self, x):
         # x: (batch_size, seq_len, hidden_size)
@@ -75,11 +76,11 @@ class SelfAttention(nn.Module):
 
 
 class Projection(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size):
         super().__init__()
-        self.dense = nn.Linear(1024, 1024)
+        self.dense = nn.Linear(hidden_size, hidden_size)
         self.dropout = nn.Dropout(0.1)
-        self.layer_norm = nn.LayerNorm(1024)
+        self.layer_norm = nn.LayerNorm(hidden_size)
 
     def forward(self, hidden_states, input_tensor):
         hidden_states = self.dense(hidden_states)
@@ -89,20 +90,21 @@ class Projection(nn.Module):
 
 
 class Attention(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size, n_heads):
         super().__init__()
-        self.self_attn = SelfAttention()
-        self.proj = Projection()
+        self.self_attn = SelfAttention(hidden_size, n_heads)
+        self.proj = Projection(hidden_size)
 
     def forward(self, hidden_states):
         self_output = self.self_attn(hidden_states)
         attention_output = self.proj(self_output, hidden_states)
         return attention_output
 
+
 # %%
 # Users can instantiate the model based on the above definition as usual.
 
-model = Attention()
+model = Attention(hidden_size=1024, n_heads=16)
 
 # %%
 # Later, we pass the model to Slapo and create a default schedule for it.
@@ -114,26 +116,32 @@ print(sch.mod)
 
 # %%
 # As we can see, Slapo works seamlessly with the PyTorch models and preserves
-# the hierachical structure of the original model. As we have not added any
+# the hierarchical structure of the original model. As we have not added any
 # optimizations, the module is exactly the same as the original one.
+# We can easily obtain the submodules by passing the module name to the schedule.
+
+attn_sch = sch["self_attn"]
+print(attn_sch.mod)
+
+# %%
 # This is also the idea of progressive optimization -- we only apply optimizations
 # to a small part of the model at a time and do not affect other parts.
 # If no optimizations are applied, then no changes will be made to the model, which
 # is different from the traditional static graph optimization employed by deep
 # learning compilers.
-# 
+#
 # In the following, we will show how to gradually apply optimizations to the model.
 # Since the three linear layers in the SelfAttention module are independent, we
 # can merge them into a single linear layer to reduce the number of GEMM
-# operations, and thus reduce the GPU kernel launch overheads.
-# 
+# operations, and thus reduce the kernel launch overheads.
+#
 # The first thing to do is to find those three linear layers and the consequential
 # operations in the model. Slapo provides an easy-to-use API to help users
 # define the pattern and find the corresponding module or subgraph in the model.
 # We can define a subgraph pattern function as shown below. The ``call_module``
 # function will try to match a call node that satisfies the user-defined
 # constraint in the dataflow graph. The first argument specifies the name of the
-# module, where regular expression is supported, so the it can support fuzzy
+# module, where regular expression is supported, so it can support fuzzy
 # matching in this case. The latter arguments are the arguments of the call node.
 # Using this function, we can use just one line of code to match the three linear
 # layers. Also, we need to incorporate the ``view`` and ``permute`` operations,
@@ -141,20 +149,100 @@ print(sch.mod)
 
 from slapo.pattern import call_module
 
+
 def pattern(x):
     x = call_module(r"[qkv]_proj", x)
     new_shape = x.shape[:-1] + (16, -1)
     x = x.view(new_shape)
     return x.permute(0, 2, 1, 3)
 
+
 # %%
 # After defining the pattern, we can use the ``.find()`` primitive to find the
-# corresponding subgraph in the model. It will return a list of subgraphs that
-# match the pattern. In our case, there will be three subgraphs, one for each
-# linear layer and the ``view`` and ``permute`` operations.
+# corresponding subgraph in the model.
 
-subgraphs = sch.find(pattern)
-print(subgraphs)
+qkv_subgraphs = attn_sch.find(pattern)
+
+# The primitive basically does two things. First, it will `implicitly` trace the submodule
+# into a static subgraph. Currently, we use `torch.fx <https://pytorch.org/docs/stable/fx.html>`_
+# as the IR, so the traced module will become a ``torch.fx.GraphModule``, and we can also
+# see the forward function of it.
+
+print(attn_sch.mod)
 
 # %%
-# We can later define a fused QKV layer to replace the three linear layers.
+# Second, the ``.find()` primitive will return a list of subgraphs that
+# match the pattern. In our case, there will be three subgraphs, one for each
+# linear layer and the consequential ``view`` and ``permute`` operations.
+
+print(qkv_subgraphs)
+
+# %%
+# Then, we define a fused QKV module as follows and instantiate it.
+
+class FusedQKV(nn.Module):
+    def __init__(self, hidden_size, n_heads) -> None:
+        super().__init__()
+        self.n_heads = n_heads
+        self.fused_linear = nn.Linear(hidden_size, hidden_size * 3)
+
+    def permute_for_scores(self, x):
+        new_shape = x.shape[:-1] + (self.n_heads, -1)
+        x = x.view(new_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states):
+        qkv = self.fused_linear(hidden_states)
+        reshaped_qkv = self.permute_for_scores(qkv)
+        q, k, v = torch.split(reshaped_qkv, 1, dim=-1)
+        q = torch.squeeze(q, -1).contiguous()
+        k = torch.squeeze(k, -1).contiguous()
+        v = torch.squeeze(v, -1).contiguous()
+        return [q, k, v]
+
+
+fused_qkv = FusedQKV(hidden_size=1024, n_heads=16)
+
+# %%
+# We can replace the subgraphs with the fused QKV module by calling the
+# ``.replace()`` primitive. The first argument is the new module,
+# and the second argument is the subgraph to be replaced.
+# After replacing the subgraph, we can check the model again to see the
+# changes.
+
+attn_sch.replace(fused_qkv, qkv_subgraphs)
+print(attn_sch.mod)
+
+# %%
+# From the above output, we can see there is a new module called ``FusedQKV_0``
+# with 3x ``out_features`` compared to the original linear layer.
+# The corresponding forward function is also changed to leverage the fused
+# module.
+
+# %%
+# Similarly, we can directly find the core attention function and replace it
+# with a more efficient implementation.
+
+core_attn_subgraph = attn_sch.find(scaled_dot_product)
+print(core_attn_subgraph)
+
+# %%
+# We can use the ``FlashAttentionOp`` from the ``xformers`` library to replace
+# the core attention, which has been included in Slapo, so we can directly
+# import and replace the subgraph with ``FlashAttentionOp``.
+# Notice, since the ``scaled_dot_product`` function we defined above only accepts
+# the ``query``, ``key``, and ``value`` tensors, while ``FlashAttentionOp`` requires
+# five arguments, so we need to explicitly pass ``None`` to the `attention_mask``
+# argument, and set the dropout probability ``p`` to 0.1 by setting the ``concrete_args``.
+
+from slapo.op.attention import FlashAttentionOp
+
+flash_attn = FlashAttentionOp(attn_op_name="native_xformers", apply_causal_mask=False)
+attn_sch.replace(
+    flash_attn, core_attn_subgraph, concrete_args={"attention_mask": None, "p": 0.1}
+)
+print(attn_sch.mod)
+
+# %%
+# Again, the ``FlashAttentionOp`` is attached to the GraphModule, and the forward
+# function becomes much simpler to call those two submodules.

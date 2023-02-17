01d72bc2d1bfa3fd3faa40fad9190b3dd7e807d9
Cody Yu <comaniac0422@gmail.com>
2023-02-17 14:05:06 -0800

    Add param_name to shard infer type and fix consolidate (#62)

--- slapo/sharding/infer_type.py
+++ slapo/sharding/infer_type.py
@@ -1,7 +1,13 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Output type inference function registration for shardable ops."""
+# pylint: disable=unused-argument
 from torch import nn
+
+try:
+    from transformers.modeling_utils import Conv1D
+except ImportError:
+    Conv1D = None
 
 OUTPUT_INFER_FN = {}
 
@@ -10,6 +16,11 @@ def register_output_infer_fn(module_cls):
     """Register an output inference function for an op."""
 
     def decorator(rule_fn):
+        # Do nothing if the module class is None. This happens when
+        # the module to be registered is not imported.
+        if module_cls is None:
+            return rule_fn
+
         if module_cls in OUTPUT_INFER_FN:
             raise ValueError(f"{module_cls} already registered for output inference")
         OUTPUT_INFER_FN[module_cls] = rule_fn
@@ -23,7 +34,7 @@ def get_all_rules():
     return OUTPUT_INFER_FN
 
 
-def get_output_type_after_sharding(module, sharded_size, axis):
+def get_output_type_after_sharding(module, param_name, sharded_size, axis):
     """Get the output type (partition or partial) after sharding the module
     along the given axis.
 
@@ -31,6 +42,8 @@ def get_output_type_after_sharding(module, sharded_size, axis):
     ----------
     module: torch.nn.Module
         The module to shard.
+    param_name: str
+        The name of the parameter to shard.
     sharded_size: int
         The size of the sharded dimension.
     axis: int
@@ -45,12 +58,12 @@ def get_output_type_after_sharding(module, sharded_size, axis):
     """
     module_cls = module.__class__
     if module_cls in OUTPUT_INFER_FN:
-        return OUTPUT_INFER_FN[module_cls](module, sharded_size, axis)
+        return OUTPUT_INFER_FN[module_cls](module, param_name, sharded_size, axis)
     return None, None
 
 
 @register_output_infer_fn(nn.Linear)
-def _linear(module, sharded_size, axis):
+def _linear(module, param_name, sharded_size, axis):
     """Output inference for linear layer.
     It adjusts the input or output feature size to reflect the shard size,
     and returns the output type (partial or partition) after sharding.
@@ -59,6 +72,8 @@ def _linear(module, sharded_size, axis):
     ----------
     module: torch.nn.Linear
         The linear layer to shard.
+    param_name: str
+        The name of the parameter to shard.
     sharded_size: int
         The size of the sharded dimension.
     axis: int
@@ -79,7 +94,7 @@ def _linear(module, sharded_size, axis):
 
 
 @register_output_infer_fn(nn.Conv2d)
-def _conv2d(module, sharded_size, axis):
+def _conv2d(module, param_name, sharded_size, axis):
     """Output inference for conv2d layer.
     It adjusts the input or output channel number to reflect the shard size,
     and returns the output type (partial or partition) after sharding.
@@ -88,6 +103,8 @@ def _conv2d(module, sharded_size, axis):
     ----------
     module: torch.nn.Conv2d
         The layer to shard.
+    param_name: str
+        The name of the parameter to shard.
     sharded_size: int
         The size of the sharded dimension.
     axis: int
@@ -110,7 +127,7 @@ def _conv2d(module, sharded_size, axis):
 
 
 @register_output_infer_fn(nn.BatchNorm2d)
-def _batchnorm2d(module, sharded_size, axis):
+def _batchnorm2d(module, param_name, sharded_size, axis):
     """Output inference for BatchNorm2d layer.
     It adjusts the feature number to reflect the shard size,
     and returns the output type (partition along axis=1).
@@ -119,6 +136,8 @@ def _batchnorm2d(module, sharded_size, axis):
     ----------
     module: torch.nn.BatchNorm2d
         The layer to shard.
+    param_name: str
+        The name of the parameter to shard.
     sharded_size: int
         The size of the sharded dimension.
     axis: int
@@ -133,3 +152,34 @@ def _batchnorm2d(module, sharded_size, axis):
         raise ValueError("BatchNorm2d only supports sharding on axis 0")
     module.num_features = sharded_size
     return ("partition", 1)
+
+
+@register_output_infer_fn(Conv1D)
+def _conv1d(module, param_name, sharded_size, axis):
+    """Output inference for Conv1D layer. Note that Conv1D has a transposed
+    weight (input features, output features) compared to Linear.
+    It adjusts the input or output feature size to reflect the shard size,
+    and returns the output type (partial or partition) after sharding.
+
+    Parameters
+    ----------
+    module: torch.nn.Linear
+        The linear layer to shard.
+    param_name: str
+        The name of the parameter to shard.
+    sharded_size: int
+        The size of the sharded dimension.
+    axis: int
+        The axis to shard on.
+
+    Returns
+    -------
+    tuple
+        The output type and the axis (if the output is partitioned).
+    """
+    if axis == 1 or param_name == "bias":
+        module.nf = sharded_size
+        # Note that the axis is the axis of the output.
+        return ("partition", 1)
+    # axis == 0
+    return ("partial", None)

90288e84efe7ca9208b482fe1ae2b3351e2d1b9f
chhzh123 <hc676@cornell.edu>
2023-03-06 19:23:31 +0000

    Add license header

--- examples/gpt2/run.sh
+++ examples/gpt2/run.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 deepspeed deepspeed_hf.py --batch_size 32 --micro_batch_size 4 --model_name gpt2-xl --iter_nums 20 --hidden-size 2048 --nlayers 24 --num-attn-heads 16 --dropout 0.1 --activation_function gelu --seq_len 2048 --pmp 2 --tmp 2 --checkpoint 1.0

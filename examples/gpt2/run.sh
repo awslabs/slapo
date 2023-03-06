026d886e3cc938b1372abf232cd78863a623ab7d
chhzh123 <hc676@cornell.edu>
2023-03-06 18:54:03 +0000

    Add test script

---
+++ examples/gpt2/run.sh
@@ -0,0 +1,1 @@
+deepspeed deepspeed_hf.py --batch_size 32 --micro_batch_size 4 --model_name gpt2-xl --iter_nums 20 --hidden-size 2048 --nlayers 24 --num-attn-heads 16 --dropout 0.1 --activation_function gelu --seq_len 2048 --pmp 2 --tmp 2 --checkpoint 1.0

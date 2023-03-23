63095409c59c5d195b0005fd7285fc04060563d1
chhzh123 <hc676@cornell.edu>
2023-03-23 05:25:06 +0000

    Add v0.0.3

--- slapo/version.py
+++ slapo/version.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 """Version information for the package.
 This file should not be changed manually. The steps of bumping a version are:
-1. Create a new tag (e.g., v0.0.2) and a release in the Github repo.
+1. Create a new tag (e.g., v0.0.3) and a release in the Github repo.
 2. Run git fetch --tags to fetch the new tag.
 3. Run python update_version.py to update the version information in this file.
 """
-__version__ = "0.0.2.dev0"
+__version__ = "0.0.3.dev0"

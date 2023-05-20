813e2bfc0f2a831f2a7bdb6bed558dd05c92e0c9
chhzh123 <hc676@cornell.edu>
2023-05-20 06:16:24 +0000

    Use delay init

--- tests/test_reshard.py
+++ tests/test_reshard.py
@@ -71,12 +71,12 @@ def test_schemes(init_dist):
 
     input_tensor_verf = torch.randn(bs, seq_len, hidden_size).to(device=device)
 
-    mlp = MLP(hidden_size)
+    with slapo.init_empty_weights():
+        mlp = MLP(hidden_size)
 
     # 1. Naive. RR * RR -> RR; RR * RR -> RR
     logger.info("===== 1. Naive RR =====", ranks=0)
-    mlp_naive = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_naive)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         # do nothing
         pass
@@ -84,8 +84,7 @@ def test_schemes(init_dist):
 
     # 2. RR * RS -> RS; RS -> SR (reshard); SR * RR -> SR; SR -> RR (reshard)
     logger.info("===== 2. RS -> SR =====", ranks=0)
-    mlp_2 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_2)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].shard("weight", axis=0)
         sch["fc1"].shard("bias", axis=0)
@@ -95,8 +94,7 @@ def test_schemes(init_dist):
 
     # 3. RR * RS -> RS; RS -> SR (reshard); SR -> RS (reshard); RS * SR -> RR (with all reduce)
     logger.info("===== 3. SR -> RS =====", ranks=0)
-    mlp_3 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_3)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].shard("weight", axis=0)
         sch["fc1"].shard("bias", axis=0)
@@ -108,8 +106,7 @@ def test_schemes(init_dist):
 
     # 4. Megatron. RR * RS -> RS; RS * SR -> RR (with all reduce)
     logger.info("===== 4. Megatron =====", ranks=0)
-    mlp_4 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_4)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].shard("weight", axis=0)
         sch["fc1"].shard("bias", axis=0)
@@ -119,8 +116,7 @@ def test_schemes(init_dist):
 
     # 5. RR * RS -> RS; RS -> RR (reshard); RR * RS -> RS; RS -> RR (reshard)
     logger.info("===== 5. RR * RS -> RS =====", ranks=0)
-    mlp_5 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_5)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].shard("weight", axis=0)
         sch["fc1"].shard("bias", axis=0)
@@ -132,8 +128,7 @@ def test_schemes(init_dist):
 
     # 6. RR * RR -> RR; RR -> RS (reshard); RS * SR -> RR (with all reduce)
     logger.info("===== 6. RR -> RS =====", ranks=0)
-    mlp_6 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_6)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].sync("fwd_post", sync_op_or_fn="RR->RS")
         sch["fc2"].shard("weight", axis=1)
@@ -142,8 +137,7 @@ def test_schemes(init_dist):
 
     # 7. RR * RR -> RR; RR -> SR (reshard); SR * RR -> SR; SR -> RR (reshard)
     logger.info("===== 7. RR -> SR =====", ranks=0)
-    mlp_7 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_7)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].sync("fwd_post", sync_op_or_fn="RR->SR")
         sch["fc2"].sync("fwd_post", sync_op_or_fn="SR->RR")
@@ -151,8 +145,7 @@ def test_schemes(init_dist):
 
     # 8. RR -> SR (reshard); SR * RR -> SR; SR * RR -> SR; SR -> RR (reshard)
     logger.info("===== 8. RR -> RS =====", ranks=0)
-    mlp_8 = copy.deepcopy(mlp).to(device=device)
-    sch = slapo.create_schedule(mlp_8)
+    sch = slapo.create_schedule(copy.deepcopy(mlp))
     with slapo.Verify(sch, [input_tensor_verf]):
         sch["fc1"].sync("fwd_pre", sync_op_or_fn="RR->SR")
         sch["fc2"].sync("fwd_post", sync_op_or_fn="SR->RR")
@@ -176,3 +169,4 @@ if __name__ == "__main__":
     mods = test_schemes(None)
     for i, model in enumerate(mods):
         perf_model(model, inp, i)
+        del model

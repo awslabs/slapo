87a43f48e0e2a51727bdb662cb2e0e042e5ecaa5
chhzh123 <hc676@cornell.edu>
2023-05-20 01:08:11 +0000

    Fix SR->RS

--- slapo/sharding/reshard_ops.py
+++ slapo/sharding/reshard_ops.py
@@ -106,6 +106,7 @@ def reshard_RS_to_RR(in_tensor, group):
 # 6. RS -> SR
 def reshard_RS_to_SR(in_tensor, group):
     # (bs, seq, hs/p) => (bs, seq/p, hs)
+    world_size = dist.get_world_size(group)
     # Since all_to_all can only chunk the 0th dimension, we need to permute the tensor
     # to make the 0th dimension the one we want to send data.
     dims = list(range(len(in_tensor.shape)))
@@ -113,24 +114,20 @@ def reshard_RS_to_SR(in_tensor, group):
     # (seq, bs, hs/p)
     in_tensor = in_tensor.permute(dims).contiguous()
     in_shape = in_tensor.shape
-    output = torch.empty(in_shape, dtype=in_tensor.dtype, device=in_tensor.device)
-    # (p*seq/p, bs, hs/p)
-    dist.all_to_all_single(output, in_tensor, group=group)
+    in_tensor_lst = list(in_tensor.chunk(world_size))
+    out_tensor_lst = list(
+        torch.empty(in_shape, dtype=in_tensor.dtype, device=in_tensor.device).chunk(
+            world_size
+        )
+    )
+    # (p, seq/p, bs, hs/p)
+    dist.all_to_all(out_tensor_lst, in_tensor_lst, group=group)
+    # (seq/p, bs, hs)
+    output = torch.cat(out_tensor_lst, dim=-1)
     dims = list(range(1, len(in_tensor.shape) - 1)) + [0, -1]
     # Permute back to the original layout
-    # (bs, p*seq/p, hs/p)
-    output = output.permute(dims).contiguous()
-    # (bs, p, seq/p, hs/p)
-    out_shape = list(output.shape)
-    world_size = dist.get_world_size(group)
-    output = output.view(out_shape[:-2] + [world_size, out_shape[-2] // world_size, out_shape[-1]])
-    dims = list(range(len(output.shape)))[:-3] + [-2, -3, -1]
-    # (bs, seq/p, p, hs/p)
-    output = output.permute(dims)
-    out_shape = list(output.shape)
-    out_shape = list(out_shape[:-2] + [-1])
     # (bs, seq/p, hs)
-    output = output.view(out_shape)
+    output = output.permute(dims)
     return output
 
 
@@ -152,30 +149,30 @@ def reshard_SR_to_RR(in_tensor, group):
 
 # 8. SR -> RS
 def reshard_SR_to_RS(in_tensor, group):
+    # (bs, seq/p, hs) => (bs, seq, hs/p)
+    world_size = dist.get_world_size(group)
+    # Since all_to_all can only chunk the 0th dimension, we need to permute the tensor
+    # to make the 0th dimension the one we want to send data.
+    dims = list(range(len(in_tensor.shape)))
+    dims = [-1] + dims[:-1]
+    # (hs, bs, seq/p)
+    in_tensor = in_tensor.permute(dims).contiguous()
     in_shape = in_tensor.shape
-    chunk_shape = list(in_shape[:-1]) + [in_shape[-1] // dist.get_world_size(group)]
-
-    splitted_tensor = torch.split(
-        in_tensor, in_shape[-1] // dist.get_world_size(group), dim=-1
+    in_tensor_lst = list(in_tensor.chunk(world_size))
+    out_tensor_lst = list(
+        torch.empty(in_shape, dtype=in_tensor.dtype, device=in_tensor.device).chunk(
+            world_size
+        )
     )
-
-    for i in range(dist.get_world_size(group)):
-        send_tensor = splitted_tensor[i].contiguous()
-
-        if dist.get_rank() != i:
-            dist.gather(send_tensor, dst=i, async_op=True, group=group)  # send
-        else:
-            gather_list = [
-                torch.empty(chunk_shape, dtype=in_tensor.dtype, device=in_tensor.device)
-                for _ in range(dist.get_world_size(group))
-            ]
-            handle = dist.gather(
-                send_tensor, gather_list, dst=i, async_op=True, group=group
-            )  # recv
-    handle.wait()
-
-    ret = torch.cat(gather_list, dim=-2)
-    return ret
+    # (p, hs/p, bs, seq/p)
+    dist.all_to_all(out_tensor_lst, in_tensor_lst, group=group)
+    # (hs/p, bs, seq)
+    output = torch.cat(out_tensor_lst, dim=-1)
+    dims = list(range(1, len(in_tensor.shape))) + [0]
+    # Permute back to the original layout
+    # (bs, seq, hs/p)
+    output = output.permute(dims)
+    return output
 
 
 # 9. SR -> SR

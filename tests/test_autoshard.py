9a326efc69dc1787d9ff128939a503fe9b2777d1
chhzh123 <hc676@cornell.edu>
2023-05-23 04:02:01 +0000

    Add attn test

--- tests/test_autoshard.py
+++ tests/test_autoshard.py
@@ -12,6 +12,7 @@ import torch.nn.functional as F
 
 import slapo
 from slapo.logger import get_logger
+from slapo.sharding import Solver
 
 logger = get_logger(__name__)
 
@@ -43,8 +44,6 @@ def test_mlp():
     sch.trace()
     assert isinstance(sch.mod, fx.GraphModule)
 
-    from slapo.sharding import Solver
-
     sol = Solver(sch.mod, p=p)
     results, max_cost = sol.solve([torch.randn(bs, seq_len, hidden_size)])
     # fc1: SRxRR->SR
@@ -53,8 +52,38 @@ def test_mlp():
     assert results["fc1_1"] == 0
     assert results["fc2_0"] == 2
     assert results["fc2_1"] == 0
-    assert max_cost == seq_len * hidden_size / p
+    assert max_cost == (bs * seq_len * hidden_size / p + 1)
+
+
+def test_attn():
+    from transformers import BertLMHeadModel, AutoConfig
+    import inspect
+
+    config = AutoConfig.from_pretrained("bert-large-uncased")
+    with slapo.init_empty_weights():
+        model = BertLMHeadModel(config)
+    logger.info(config, ranks=0)
+
+    sch = slapo.create_schedule(model)
+    input_names = ["hidden_states"]
+    i = 0
+    subsch = sch[f"bert.encoder.layer.{i}"]
+    sig = inspect.signature(subsch.mod.forward)
+    concrete_args = {
+        p.name: p.default for p in sig.parameters.values() if p.name not in input_names
+    }
+    subsch.trace(
+        recursive=False,
+        flatten=True,
+        tracer="pytorch",
+        concrete_args=concrete_args,
+    )
+    logger.info(subsch.mod.graph, ranks=0)
+
+    sol = Solver(subsch.mod, p=p)
+    results, max_cost = sol.solve([torch.randn(bs, seq_len, hidden_size)])
 
 
 if __name__ == "__main__":
     test_mlp()
+    test_attn()

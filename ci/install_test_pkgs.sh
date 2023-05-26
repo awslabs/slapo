285e4a7c2c5241b4b9e56352e3c66dfb3db52579
chhzh123 <hc676@cornell.edu>
2023-05-26 21:26:23 +0000

    Update dependency

--- ci/install_test_pkgs.sh
+++ ci/install_test_pkgs.sh
@@ -5,3 +5,4 @@
 python3 -m pip install black==22.10.0
 python3 -m pip install transformers==4.25.1 --no-deps
 python3 -m pip install pylint==2.14.0 astroid==2.11.6 mock==4.0.3
+python3 -m pip install z3-solver tabulate

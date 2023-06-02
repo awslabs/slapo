e32c102d266739ac8b1fe7a09fdf2ce2cbefeb0a
chhzh123 <hc676@cornell.edu>
2023-06-02 01:17:12 +0000

    Fix possible privilege issue

--- ci/install_test_pkgs.sh
+++ ci/install_test_pkgs.sh
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 python3 -m pip install black==22.10.0
-python3 -m pip install transformers==4.28.1 datasets
+python3 -m pip install transformers==4.28.1 huggingface-hub tokenizers datasets
 python3 -m pip install pylint==2.14.0 astroid==2.11.6 mock==4.0.3

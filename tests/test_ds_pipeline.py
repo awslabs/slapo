4aedbb3d1878f25a1725c96f109eee090eddb708
chhzh123 <hc676@cornell.edu>
2023-06-10 17:54:57 +0000

    Add DP

--- tests/test_ds_pipeline.py
+++ tests/test_ds_pipeline.py
@@ -29,20 +29,24 @@ class Model(nn.Module):
         return x
 
 
-def test_pipeline_2stages():
+def test_pipeline_2stages_pp_dp():
     deepspeed.init_distributed(dist_backend="nccl")
     local_rank = int(os.environ["LOCAL_RANK"])
     torch.cuda.set_device(local_rank)
     with slapo.init_empty_weights():
         model = Model(12)
-    topology, group = create_dist_group_for_pipeline(num_pp=2, num_mp=1)
+    # If total number of devices is 4, then num_dp = 2
+    num_pp = 2
+    num_mp = 1
+    num_dp = dist.get_world_size() // (num_pp * num_mp)
+    topology, group = create_dist_group_for_pipeline(num_pp=num_pp, num_mp=num_mp)
     sch = slapo.create_schedule(model, group=group)
     sch.trace_until("")
 
     bs = 8
     ds_config_dict = get_ds_config(
         batch_size=bs,
-        micro_batch_size_per_gpu=bs,
+        micro_batch_size_per_gpu=bs // num_dp,
         fp16=False,
     )
     inp = torch.randn(bs, 10, device=dist.get_rank())
@@ -58,7 +62,7 @@ def test_pipeline_2stages():
         sch["layers.5"].cut_pipeline_stage()
 
 
-def test_pipeline_4stages():
+def test_pipeline_4stages_pp():
     deepspeed.init_distributed(dist_backend="nccl")
     local_rank = int(os.environ["LOCAL_RANK"])
     torch.cuda.set_device(local_rank)
@@ -91,4 +95,4 @@ def test_pipeline_4stages():
 
 if __name__ == "__main__":
     # pytest.main([__file__])
-    test_pipeline_4stages()
+    test_pipeline_2stages_pp_dp()

6b3aa7c0a6620351174a5b6e41a3b0796bf0481d
chhzh123 <hc676@cornell.edu>
2023-06-10 17:47:35 +0000

    Add 4stages

--- tests/test_ds_pipeline.py
+++ tests/test_ds_pipeline.py
@@ -19,9 +19,9 @@ import torch.nn.functional as F
 
 
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, num_layers=12):
         super().__init__()
-        self.layers = nn.ModuleList([nn.Linear(10, 10) for _ in range(12)])
+        self.layers = nn.ModuleList([nn.Linear(10, 10) for _ in range(num_layers)])
 
     def forward(self, x):
         for layer in self.layers:
@@ -29,12 +29,12 @@ class Model(nn.Module):
         return x
 
 
-def test_pipeline():
+def test_pipeline_2stages():
     deepspeed.init_distributed(dist_backend="nccl")
     local_rank = int(os.environ["LOCAL_RANK"])
     torch.cuda.set_device(local_rank)
     with slapo.init_empty_weights():
-        model = Model()
+        model = Model(12)
     topology, group = create_dist_group_for_pipeline(num_pp=2, num_mp=1)
     sch = slapo.create_schedule(model, group=group)
     sch.trace_until("")
@@ -58,6 +58,37 @@ def test_pipeline():
         sch["layers.5"].cut_pipeline_stage()
 
 
+def test_pipeline_4stages():
+    deepspeed.init_distributed(dist_backend="nccl")
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(local_rank)
+    with slapo.init_empty_weights():
+        model = Model(12)
+    topology, group = create_dist_group_for_pipeline(num_pp=4, num_mp=1)
+    sch = slapo.create_schedule(model, group=group)
+    sch.trace_until("")
+
+    bs = 8
+    ds_config_dict = get_ds_config(
+        batch_size=bs,
+        micro_batch_size_per_gpu=bs,
+        fp16=False,
+    )
+    inp = torch.randn(bs, 10, device=dist.get_rank())
+    label = torch.randint(0, 10, (bs,), dtype=torch.long, device=dist.get_rank())
+    with slapo.Verify(
+        sch,
+        example_inputs=[inp],
+        example_outputs=label,
+        loss_fn=F.cross_entropy,
+        topology=topology,
+        config=ds_config_dict,
+    ):
+        sch["layers.2"].cut_pipeline_stage()
+        sch["layers.5"].cut_pipeline_stage()
+        sch["layers.8"].cut_pipeline_stage()
+
+
 if __name__ == "__main__":
     # pytest.main([__file__])
-    test_pipeline()
+    test_pipeline_4stages()

bb2ba5f3d6b28c90b82e0f5fe2822f6e847d6d27
chhzh123 <hc676@cornell.edu>
2023-06-11 22:04:59 +0000

    Add GPT2 PP test (not done yet)

--- tests/test_ds_pipeline.py
+++ tests/test_ds_pipeline.py
@@ -275,9 +275,87 @@ def test_bert_2stages_pp():
         sch["bert.encoder.layer.11"].cut_pipeline_stage()
 
 
+def test_gpt2_2stages_pp():
+    deepspeed.init_distributed(dist_backend="nccl")
+    if dist.get_world_size() != 2:
+        logger.info("This test requires 2 GPUs.")
+        return
+    local_rank = int(os.environ["LOCAL_RANK"])
+    torch.cuda.set_device(local_rank)
+
+    from transformers import GPT2LMHeadModel, AutoConfig
+
+    config = AutoConfig.from_pretrained("gpt2-xl")
+    config.use_cache = False
+    with slapo.init_empty_weights():
+        model = GPT2LMHeadModel(config)
+
+    num_pp = 2
+    num_mp = 1
+    num_dp = dist.get_world_size() // (num_pp * num_mp)
+    topology, group = create_dist_group_for_pipeline(num_pp=num_pp, num_mp=num_mp)
+    sch = slapo.create_schedule(model, group=group)
+    input_names = ["input_ids", "attention_mask", "token_type_ids"]
+    sig = inspect.signature(sch.mod.forward)
+    concrete_args = {
+        p.name: p.default for p in sig.parameters.values() if p.name not in input_names
+    }
+
+    loss_fct = nn.CrossEntropyLoss()
+
+    def loss_fn(outputs, labels):
+        # (bs, seq, vocab)
+        if isinstance(outputs, torch.Tensor):
+            # DS PP output has already removed the data structure
+            prediction_scores = outputs
+        else:
+            prediction_scores = outputs["logits"]
+        shifted_prediction_scores = prediction_scores[:, :-1, :].contiguous()
+        labels = labels[:, 1:].contiguous()
+        lm_loss = loss_fct(
+            shifted_prediction_scores.view(-1, config.vocab_size), labels.view(-1)
+        )
+        return lm_loss
+
+    bs = 2
+    seq_len = 512
+    micro_bs = bs // num_dp
+    ds_config_dict = get_ds_config(
+        batch_size=bs,
+        micro_batch_size_per_gpu=micro_bs,
+        fp16=False,
+    )
+    device = "cuda"
+    input_ids = torch.ones(micro_bs, seq_len, dtype=torch.long, device=device)
+    attention_mask = torch.ones(
+        micro_bs, seq_len, dtype=torch.float32, requires_grad=False, device=device
+    )
+    position_ids = torch.ones(
+        micro_bs, seq_len, dtype=torch.long, requires_grad=False, device=device
+    )
+    labels = torch.randint(
+        0, 10, (micro_bs, seq_len), dtype=torch.long, device=sch.rank
+    )
+    with slapo.Verify(
+        sch,
+        # (input_ids, past_key_values, attention_mask, token_type_ids, position_ids)
+        example_inputs=[input_ids, None, attention_mask, None, position_ids],
+        example_outputs=labels,
+        loss_fn=loss_fn,
+        topology=topology,
+        config=ds_config_dict,
+        init_weights=model._init_weights,
+    ):
+        sch.trace_until(
+            "transformer", tracer="huggingface", concrete_args=concrete_args
+        )
+        sch["transformer.h.23"].cut_pipeline_stage()
+
+
 if __name__ == "__main__":
     test_pipeline_2stages_pp_dp()
     test_pipeline_2stages_pp_tp()
     test_pipeline_4stages_pp()
     test_pipeline_2stages_pp_tp_dp()
     test_bert_2stages_pp()
+    test_gpt2_2stages_pp()
